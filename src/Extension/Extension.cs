00724937ec5d55e672ed439757ab12f66fe3248f
Dmitry Sikorsky <dmitry@sikorsky.pro>
2017-02-14 18:38:34 +0200

    Added project content

---
+++ src/Extension/Extension.cs
@@ -0,0 +1,18 @@
+﻿// Copyright © 2017 Dmitry Sikorsky. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using ExtCore.Infrastructure;
+
+namespace Extension
+{
+  public class Extension : ExtensionBase
+  {
+    public override string Name
+    {
+      get
+      {
+        return "Some name";
+      }
+    }
+  }
+}
---
+++ src/WebApplication/Startup.cs
@@ -0,0 +1,44 @@
+﻿// Copyright © 2017 Dmitry Sikorsky. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Linq;
+using ExtCore.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication
+{
+  public class Startup : ExtCore.WebApplication.Startup
+  {
+    public Startup(IServiceProvider serviceProvider)
+      : base (serviceProvider)
+    {
+      this.serviceProvider.GetService<ILoggerFactory>().AddConsole();
+
+      IConfigurationBuilder configurationBuilder = new ConfigurationBuilder()
+        .SetBasePath(this.serviceProvider.GetService<IHostingEnvironment>().ContentRootPath)
+        .AddJsonFile("config.json", optional: true, reloadOnChange: true);
+
+      this.configurationRoot = configurationBuilder.Build();
+    }
+
+    public override void ConfigureServices(IServiceCollection services)
+    {
+      base.ConfigureServices(services);
+    }
+
+    public override void Configure(IApplicationBuilder applicationBuilder)
+    {
+      base.Configure(applicationBuilder);
+      applicationBuilder.Run(async (context) =>
+      {
+        await context.Response.WriteAsync(ExtensionManager.Extensions.First().Name);
+      });
+    }
+  }
+}
